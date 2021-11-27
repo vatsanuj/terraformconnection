cafa714a824182a54994b533556d841926d7c7e3
Anuj Sharma <91746087+vatsanuj@users.noreply.github.com>
2021-11-27 22:43:40 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,5 @@ provider "google" {
   # Configuration options
   project = "sre-terraform-8080"
   region = "us-central1"
-  zone = "us-central1-a"
-  credentials = "key.json"
+  zone = "us-central1-a
 }
