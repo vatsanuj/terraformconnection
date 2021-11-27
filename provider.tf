c68631fb087f3736c69933300562d3c73eb61731
Anuj Sharma <91746087+vatsanuj@users.noreply.github.com>
2021-11-27 22:41:45 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.85.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "sre-terraform-8080"
+  region = "us-central1"
+  zone = "us-central1-a"
+  credentials = "key.json"
+}
